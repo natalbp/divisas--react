d661c086084a61717daa3b3ae4fa9abb34c054ed
Nat <nataliaberpino@gmail.com>
2026-03-03 08:24:16 -0500

    hola 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'react'
+import { use, useEffect, useState } from 'react'
+import './estilos.css'
 
 const App = () => {
-
 const [divisas, setDivisas] = useState([])
+const [seleccion, setSeleccion] = useState("")
+const[divisaDefinitiva, setDivisaDefinitiva]= useState(null)
+const [valorInput, setValorInput] = useState(null)
 
   useEffect ( () => {
    consultar()
@@ -12,22 +15,52 @@ const [divisas, setDivisas] = useState([])
 const consultar = async () => {
   let url ="https://co.dolarapi.com/v1/cotizaciones"
   const resultado = await fetch (url)
-  setDivisas(resultado.json())
+  const data = await resultado.json()
+  console.log(data)
+  setDivisas(data)
+}
+
+const cambioDivisa = (idDivisa) => {
+  console.log("Divisa seleccionada ", idDivisa)
+  setSeleccion(idDivisa)
+  setDivisaDefinitiva(buscarDivisa(idDivisa))
+  console.log("Divisa encontrada ", divisa)
+}
+
+const buscarDivisa= (idDivisa) => {
+  let divisaEncontrada = divisas.find( divisa => divisa.moneda === idDivisa)
+  return divisaEncontrada
+}
+
+const cambioInput = (nuevoValor) => {
+  
 }
 
   return (
+
     <div className="contenedor">
         <h1>Convertir desde COP</h1>
-        <input type="number" id="valor" placeholder="Cantidad en pesos (COP)"/>
+        <input value={valorInput} type="number" id="valor" placeholder="Cantidad en pesos (COP)"
+        onChange={ (evento) => cambioInput(evento.target.value)}/>
         
-        <select id="opcionesDivisas">
+        <select id="opcionesDivisas" 
+        onChange={ (evento) => cambioDivisa(evento.target.value)}>
             <option value="">Cargando divisas...</option>
+            { divisas &&
+              divisas.map( divisa => (
+                <option value={divisa.moneda}>{divisa.nombre}</option>
+              ))
+            }
         </select>
 
-        <button onclick="convertir()">Convertir</button>
-        <p id="resultado"></p>
+        {/*<button>Convertir</button>*/}
+        <p id="resultado">
+           {valorInput / divisaDefinitiva?.ultimoCierre}
+        </p>
     </div>
   )
-}
 
+
+}
 export default App
+  
